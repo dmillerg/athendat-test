79b811ec42a7a88bde4355ae4b5ebffb5301b551
dmillerg <dmillergg@gmail.com>
2024-07-01 18:13:27 -0400

    FIX: cleaning code

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { ProfileComponent } from '../profile/profile.component';
+import { Component, OnInit, inject } from '@angular/core';
 import { CardComponent } from '../../../../shared/components/card/card.component';
 import { IntroduceComponent } from '../introduce/introduce.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
@@ -10,12 +8,9 @@ import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { BlogComponent } from '../blog/blog.component';
 import { TestimonialComponent } from '../testimonial/testimonial.component';
 import { ContactComponent } from '../contact/contact.component';
-import { HeaderComponent } from '../../../../shared/components/header/header.component';
-import { FooterComponent } from '../../../../shared/components/footer/footer.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { scrollTo } from '../../../../core/functions/scroll-to.function';
 import { SkillComponent } from '../skill/skill.component';
-import { CommonModule } from '@angular/common';
 import { home } from './constant/home';
 import { ICONBLOG } from '../../../../core/constant/icon-blog';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -24,8 +19,6 @@ import { DomSanitizer } from '@angular/platform-browser';
   selector: 'app-home',
   standalone: true,
   imports: [
-    SidebarComponent,
-    ProfileComponent,
     CardComponent,
     IntroduceComponent,
     AboutMeComponent,
@@ -36,9 +29,6 @@ import { DomSanitizer } from '@angular/platform-browser';
     BlogComponent,
     TestimonialComponent,
     ContactComponent,
-    HeaderComponent,
-    FooterComponent,
-    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -47,26 +37,12 @@ export class HomeComponent implements OnInit {
 
   _activatedRoute = inject(ActivatedRoute);
   _router = inject(Router);
-  activo = signal('');
+  _sanitized = inject(DomSanitizer);
   home = home;
-  iconBlog = this.sanitized.bypassSecurityTrustHtml(ICONBLOG)
-
-  constructor(private sanitized: DomSanitizer) { }
+  iconBlog = this._sanitized.bypassSecurityTrustHtml(ICONBLOG)
 
   ngOnInit(): void {
     const route = this._activatedRoute.snapshot.params['route'];
     scrollTo(route)
-  }
-
-  onScroll(event: Event) {
-    const cards = document.querySelectorAll('app-card');
-    for (let i = 0; i < cards.length; i++) {
-      const cardRect = cards[i].getBoundingClientRect();
-      if (cardRect.top >= 0 && cardRect.top <= ((event.target as HTMLElement).scrollHeight - 10)) {
-        this.activo.set(cards[i].id);
-        this._router.navigate([`home/${cards[i].id}`])
-        break; // Salir del bucle si se encuentra la tarjeta visible
-      }
-    }
   }
 }
