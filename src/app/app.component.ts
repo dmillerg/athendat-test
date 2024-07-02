a5d490916ea8d12bad891eb19ed8ba7d763896e2
dmillerg <dmillergg@gmail.com>
2024-07-02 02:35:27 -0400

    ADD: follow cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,16 @@ import { LoadingComponent } from './shared/components/loading/loading.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ThemeComponent, ProfileComponent, HeaderComponent, SidebarComponent, FooterComponent, SidebarSmComponent, LoadingComponent],
+  imports: [
+    RouterOutlet,
+    ThemeComponent,
+    ProfileComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    SidebarSmComponent,
+    LoadingComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -20,8 +29,11 @@ export class AppComponent {
   title = 'athendat-test';
   _router = inject(Router);
   _dataService = inject(DataService);
+  _el = inject(ElementRef);
   open: boolean = false;
   loading: boolean = true;
+  private circle!: HTMLElement;
+  private circle2!: HTMLElement;
 
   @ViewChild('scroll') scroll!: ElementRef
 
@@ -46,5 +58,43 @@ export class AppComponent {
       top: 0,
       behavior: 'smooth'
     })
+  }
+
+
+  ngOnInit() {
+    this.circle = document.createElement('div');
+    this.circle.classList.add('w-6');
+    this.circle.classList.add('h-6');
+    this.circle.classList.add('bg-transparent');
+    this.circle.classList.add('border');
+    this.circle.classList.add('dark:border-platinum');
+    this.circle.classList.add('border-colorSilver');
+    this.circle.style.position = 'absolute';
+    this.circle.style.zIndex = '999999';
+
+    this.circle.style.borderRadius = '50%';
+    this.circle.style.pointerEvents = 'none';
+
+    this.circle2 = document.createElement('div');
+    this.circle2.classList.add('w-1');
+    this.circle2.classList.add('h-1');
+    this.circle2.classList.add('bg-transparent');
+    this.circle2.classList.add('border');
+    this.circle2.classList.add('dark:border-platinum');
+    this.circle2.classList.add('border-colorSilver');
+    this.circle2.style.position = 'absolute';
+    this.circle2.style.zIndex = '999999';
+
+    this.circle2.style.borderRadius = '50%';
+    this.circle2.style.pointerEvents = 'none';
+    this._el.nativeElement.appendChild(this.circle);
+    this._el.nativeElement.appendChild(this.circle2);
+  }
+
+  onMouseMove(event: MouseEvent) {
+    this.circle.style.left = (event.clientX - 12) + 'px';
+    this.circle.style.top = (event.clientY - 12) + 'px';
+    this.circle2.style.left = (event.clientX -3) + 'px';
+    this.circle2.style.top = (event.clientY -3) + 'px';
   }
 }
